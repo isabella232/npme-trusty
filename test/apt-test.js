e1246cd0a475c9ebe2858a53866e62d3767c64b0
bcoe <bencoe@gmail.com>
2014-08-25 14:28:24 -0700

    bootstrap an ubuntu box using ansible.

---
+++ test/apt-test.js
@@ -0,0 +1,23 @@
+var Lab = require('lab'),
+  lab = exports.lab = Lab.script(),
+  Apt = require('../lib/apt');
+
+lab.experiment('Apt', function() {
+
+  lab.experiment('installPackages', function() {
+    lab.it('installs appropriate packages', function(done) {
+      var apt = new Apt({
+        packages: ['ansible'],
+        util: {
+          exec: function(command) {
+            Lab.expect(command).to.eql('sudo apt-get install ansible');
+            done();
+          }
+        }
+      });
+
+      apt.installPackages(function() {})
+    });
+  });
+
+});
